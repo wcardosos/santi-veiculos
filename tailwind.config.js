764cad162c8b0182406f0bb992aea608ad915297
Wagner Cardoso <wagnerdev01@gmail.com>
2023-04-16 17:46:47 -0300

    Add brand colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    colors: {
+      primary: '#11566C',
+      secondary: '#FAA557',
+      ...colors,
     },
   },
-  plugins: [],
 }
