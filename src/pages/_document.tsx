15cb460c370b337ca491214f9691cdded9051b20
Wagner Cardoso <wagnerdev01@gmail.com>
2023-04-21 18:27:52 -0300

    Remove body font size handling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
-      <body className="bg-gray-50 text-xs md:text-md lg:text-lg">
+      <body className="bg-gray-50">
         <Main />
         <NextScript />
       </body>
