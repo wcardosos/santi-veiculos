b2000413cf692b05d853967b489c0aa9e9d4a2ba
Wagner Cardoso <wagnerdev01@gmail.com>
2023-05-19 19:31:12 -0300

    Changes after self review

--- apps/company/src/services/base-cars-service.ts
+++ apps/company/src/services/base-cars-service.ts
@@ -32,6 +32,10 @@ export abstract class BaseCarsService {
   private httpProvider: HttpProvider
   private API_URL = 'https://graphql.datocms.com/'
   private API_TOKEN = '8d97b0e25fc28f9821658d0d509037'
+  private HEADERS = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${this.API_TOKEN}`,
+  }
 
   constructor(httpProvider: HttpProvider) {
     this.httpProvider = httpProvider
@@ -39,15 +43,12 @@ export abstract class BaseCarsService {
 
   protected async requestData<T>(
     query: string,
-    variables: unknown,
+    variables: unknown, // Tiṕar quando for necessário utilizar essas variables
   ): Promise<T> {
     const { data } = await this.httpProvider.post(
       this.API_URL,
       { query, variables },
-      {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${this.API_TOKEN}`,
-      },
+      this.HEADERS,
     )
 
     return data
