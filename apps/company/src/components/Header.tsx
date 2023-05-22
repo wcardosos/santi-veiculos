ac2430461aa18979830187452425b515bafdeb6e
Wagner Cardoso <wagnerdev01@gmail.com>
2023-05-22 20:46:58 -0300

    Refact nav link classnames

--- apps/company/src/components/Header.tsx
+++ apps/company/src/components/Header.tsx
@@ -8,46 +8,29 @@ export default function Header({ variant = 'primary' }: HeaderProps) {
   const textColorByVariant = variant === 'primary' ? 'text-primary' : ''
   const hoverColorByVariant =
     variant === 'primary' ? 'hover:text-teal-800' : 'hover:text-primary'
+  const navLinkClassname = `${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`
 
   return (
     <header className="flex justify-between items-center py-4 text-primary px-6 max-w-5xl mx-auto">
       <a href="#home">Logo</a>
 
       <nav className="hidden lg:flex gap-4 text-white">
-        <a
-          href="/#vehicles"
-          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
-        >
+        <a href="/#vehicles" className={navLinkClassname}>
           Veículos
         </a>
-        <a
-          href="/#differentials"
-          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
-        >
+        <a href="/#differentials" className={navLinkClassname}>
           Diferenciais
         </a>
-        <a
-          href="/#why-choose-us"
-          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
-        >
+        <a href="/#why-choose-us" className={navLinkClassname}>
           Por quê nos escolher?
         </a>
-        <a
-          href="/#services"
-          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
-        >
+        <a href="/#services" className={navLinkClassname}>
           Serviços
         </a>
-        <a
-          href="/#contact"
-          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
-        >
+        <a href="/#contact" className={navLinkClassname}>
           Contato
         </a>
-        <a
-          href="/sobre"
-          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
-        >
+        <a href="/sobre" className={navLinkClassname}>
           Sobre
         </a>
       </nav>
