413cbeb1bbdbfe2330b383c447825f8f25111b06
Wagner Cardoso <wagnerdev01@gmail.com>
2023-05-22 20:42:15 -0300

    Add header variant

--- apps/company/src/components/Header.tsx
+++ apps/company/src/components/Header.tsx
@@ -1,42 +1,52 @@
-export default function Header() {
+type HeaderVariant = 'primary' | 'transparent'
+
+interface HeaderProps {
+  variant?: HeaderVariant
+}
+
+export default function Header({ variant = 'primary' }: HeaderProps) {
+  const textColorByVariant = variant === 'primary' ? 'text-primary' : ''
+  const hoverColorByVariant =
+    variant === 'primary' ? 'hover:text-teal-800' : 'hover:text-primary'
+
   return (
     <header className="flex justify-between items-center py-4 text-primary px-6 max-w-5xl mx-auto">
       <a href="#home">Logo</a>
 
       <nav className="hidden lg:flex gap-4 text-white">
         <a
-          href="#vehicles"
-          className="hover:text-primary hover:font-medium cursor-pointer"
+          href="/#vehicles"
+          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
         >
           Veículos
         </a>
         <a
-          href="#differentials"
-          className="hover:text-primary hover:font-medium cursor-pointer"
+          href="/#differentials"
+          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
         >
           Diferenciais
         </a>
         <a
-          href="#why-choose-us"
-          className="hover:text-primary hover:font-medium cursor-pointer"
+          href="/#why-choose-us"
+          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
         >
           Por quê nos escolher?
         </a>
         <a
-          href="#services"
-          className="hover:text-primary hover:font-medium cursor-pointer"
+          href="/#services"
+          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
         >
           Serviços
         </a>
         <a
-          href="#contact"
-          className="hover:text-primary hover:font-medium cursor-pointer"
+          href="/#contact"
+          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
         >
           Contato
         </a>
         <a
           href="/sobre"
-          className="hover:text-primary hover:font-medium cursor-pointer"
+          className={`${textColorByVariant} ${hoverColorByVariant} hover:font-medium cursor-pointer`}
         >
           Sobre
         </a>
