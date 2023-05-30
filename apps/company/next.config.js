e46f7054515442768662e463b762577f97d348f6
Wagner Cardoso <wagnerdev01@gmail.com>
2023-05-29 21:10:59 -0300

    Improving images handling

--- apps/company/next.config.js
+++ apps/company/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.datocms-assets.com',
+        port: '',
+        pathname: '/101238/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- apps/company/src/pages/index.tsx
+++ apps/company/src/pages/index.tsx
@@ -7,6 +7,11 @@ import useServicesSection from '@/hooks/useServicesSection'
 import { Separator } from '@radix-ui/react-separator'
 import Head from 'next/head'
 import { ArrowRight } from 'phosphor-react'
+import Image from 'next/image'
+import peopleTravelingPicture from '../../public/assets/images/people-traveling.webp'
+import womanInCarPicture from '../../public/assets/images/woman-in-car.webp'
+import buyAndSellPicture from '../../public/assets/images/buy-and-sell.webp'
+import rentalPicture from '../../public/assets/images/rental.webp'
 
 export default function Home() {
   const { defaultContactLink } = useDefaultContactButtonLink()
@@ -22,11 +27,14 @@ export default function Home() {
           className="relative min-h-screen md:min-h-[60vh] lg:min-h-screen"
           id="home"
         >
-          <img
-            className="absolute w-full h-full object-cover"
-            src="/assets/images/people-traveling.jpg"
-            alt=""
-          />
+          <div className="absolute w-full h-full">
+            <Image
+              className="object-cover"
+              src={peopleTravelingPicture}
+              alt="People traveling"
+              fill
+            />
+          </div>
 
           <article className="bg-gradient-to-t from-primary to-primary/0 w-full h-full absolute text-gray-50 py-24">
             <div className="max-w-5xl mx-auto px-6">
@@ -73,11 +81,14 @@ export default function Home() {
           id="differentials"
           className="relative my-12 h-[50vh] lg:h-[80vh]"
         >
-          <img
-            className="absolute w-full h-full object-cover"
-            src="/assets/images/woman-in-car.jpg"
-            alt="Woman in car enjoying the day"
-          />
+          <div className="absolute w-full h-full">
+            <Image
+              className="object-cover"
+              src={womanInCarPicture}
+              alt="Woman in car enjoying the day"
+              fill
+            />
+          </div>
 
           <article className="absolute w-full h-full bg-gray-900/50 text-right text-gray-50 px-6 py-10 lg:py-24">
             <div className="lg:max-w-3xl mx-auto">
@@ -96,10 +107,12 @@ export default function Home() {
           <article className="grid grid-rows-2 mt-6 lg:mt-12 gap-4 lg:gap-12">
             <div className="grid grid-rows-3 lg:grid-rows-1 grid-cols-1 lg:grid-cols-3 gap-4">
               <div className="text-center">
-                <img
+                <Image
                   className="mx-auto"
                   src="/assets/svg/steering-wheel.svg"
                   alt=""
+                  width={64}
+                  height={64}
                 />
                 <h2 className="font-bold text-lg text-primary mt-4 mb-2">
                   Compra, venda e troca
@@ -110,7 +123,13 @@ export default function Home() {
                 </p>
               </div>
               <div className="text-center">
-                <img className="mx-auto" src="/assets/svg/key.svg" alt="" />
+                <Image
+                  className="mx-auto"
+                  src="/assets/svg/key.svg"
+                  alt=""
+                  width={64}
+                  height={64}
+                />
                 <h2 className="font-bold text-lg text-primary mt-4 mb-2">
                   Locação
                 </h2>
@@ -120,7 +139,13 @@ export default function Home() {
                 </p>
               </div>
               <div className="text-center">
-                <img className="mx-auto" src="/assets/svg/shield.svg" alt="" />
+                <Image
+                  className="mx-auto"
+                  src="/assets/svg/shield.svg"
+                  alt=""
+                  width={64}
+                  height={64}
+                />
                 <h2 className="font-bold text-lg text-primary mt-4 mb-2">
                   Seguro
                 </h2>
@@ -135,7 +160,13 @@ export default function Home() {
             </div>
             <div className="grid grid-rows-3 lg:grid-rows-1 grid-cols-1 lg:grid-cols-3 gap-4">
               <div className="text-center">
-                <img className="mx-auto" src="/assets/svg/money.svg" alt="" />
+                <Image
+                  className="mx-auto"
+                  src="/assets/svg/money.svg"
+                  alt=""
+                  width={64}
+                  height={64}
+                />
                 <h2 className="font-bold text-lg text-primary mt-4 mb-2">
                   Financiamento
                 </h2>
@@ -146,14 +177,26 @@ export default function Home() {
                 </p>
               </div>
               <div className="text-center">
-                <img className="mx-auto" src="/assets/svg/history.svg" alt="" />
+                <Image
+                  className="mx-auto"
+                  src="/assets/svg/history.svg"
+                  alt=""
+                  width={64}
+                  height={64}
+                />
                 <h2 className="font-bold text-lg text-primary mt-4 mb-2">
                   Laudo cautelar
                 </h2>
                 <p>Saiba tudo o que já aconteceu com seu veículo</p>
               </div>
               <div className="text-center">
-                <img className="mx-auto" src="/assets/svg/wallet.svg" alt="" />
+                <Image
+                  className="mx-auto"
+                  src="/assets/svg/wallet.svg"
+                  alt=""
+                  width={64}
+                  height={64}
+                />
                 <h2 className="font-bold text-lg text-primary mt-4 mb-2">
                   Pagamento descomplicado
                 </h2>
@@ -168,14 +211,14 @@ export default function Home() {
           className="bg-gray-900 w-full h-[80vh] lg:h-[60vh] relative my-12"
         >
           <div className="w-full h-full absolute grid grid-cols-1 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1">
-            <img
+            <Image
               className="w-full max-h-[40vh] lg:max-h-fit h-full object-cover opacity-30"
-              src="/assets/images/buy-and-sell.jpg"
+              src={buyAndSellPicture}
               alt=""
             />
-            <img
+            <Image
               className="w-full max-h-[40vh] lg:max-h-fit h-full object-cover opacity-30"
-              src="/assets/images/rental.jpg"
+              src={rentalPicture}
               alt=""
             />
           </div>
